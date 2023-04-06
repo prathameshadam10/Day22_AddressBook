b3af5019ce7d36446eb6379bd491527747df0b4f
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-04-06 10:19:08 +0530

    Update AddressBook.java

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,29 +1,56 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook {
-    public void addDetails(){
-        Contact cont = new Contact();
-        cont.setFirstName("Prathamesh");
-        System.out.println("First Name :" + cont.getFirstName());
-        cont.setLastName("Adam");
-        System.out.println("Last Name :" + cont.getLastName());
-        cont.setAddress("Kuchan Nagar");
-        System.out.println("Address :" + cont.getAddress());
-        cont.setCity("Solapur");
-        System.out.println("City :"+ cont.getCity());
-        cont.setState("Maharastra");
-        System.out.println( "State :"+ cont.getState());
-        cont.setZip("413005");
-        System.out.println("zip Code :" + cont.getZip());
-        cont.setPhoneNumber("8983930906");
-        System.out.println( "Phone Number :" + cont.getPhoneNumber());
-        cont.setEmail("prathameshadam10@gmail.com");
-        System.out.println( "Email :"+ cont.getEmail());
+    Scanner sc = new Scanner(System.in);
+    public Contact createContact(){
+        Contact person = new Contact();
+
+            System.out.println("Enter your First Name");
+            person.setFirstName(sc.nextLine());
+
+
+            System.out.println("Enter your Last Name");
+            person.setLastName(sc.nextLine());
+
+
+            System.out.println("Enter Your Address");
+            person.setAddress(sc.nextLine());
+
+
+            System.out.println("Enter your City");
+            person.setCity(sc.nextLine());
+
+
+            System.out.println("Enter your state");
+            person.setState(sc.nextLine());
+
+
+            System.out.println("Enter your Zip code");
+            person.setZip(sc.nextLine());
+
+
+            System.out.println("Enter your Contact Number");
+            person.setPhoneNumber(sc.nextLine());
+
+
+            System.out.println("Enter your Email ID");
+            person.setEmail(sc.nextLine());
+
+            return person;
+        }
+       public void addDetails(){
+        Contact cont = createContact();
+           System.out.println(cont);
+           System.out.println("Contacts added Successfully");
+        }
+
+
 
 
     }
 
-    }
 
 
 
